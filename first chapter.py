22662228064715ed72dd4ad247596dff145c6c1e
Prakhar Singh Rana <156541082+prakharsinghrana23@users.noreply.github.com>
2024-10-01 22:57:40 +0530

    Add files via upload

---
+++ first chapter.py
@@ -0,0 +1,9 @@
+print("Hello Bhai")
+name=input("What is your name ")
+print("Hello " + name)
+father=input ("what is your Fathers name ")
+print("S/o " + father)
+input3=input("What is your Mother name ")
+print("Mother Name - " + input3)
+import pyjokes
+print(pyjokes.get_joke())                                                               
